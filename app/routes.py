d408db7d7d67c0c6202140dee1af1c97357dd272
Maciejo <Maciejko@gmail.com>
2018-12-01 17:45:16 +0100

    dodano duzo zeczy

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,16 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="Home Page")
 
-@app.route('/user/<name>')
-def getUser(name):
-    return render_template('user.html', name=name)
+@app.route('/galeria')
+def getGalery():
+    listaObrazkow = [
+        "https://whatnext.pl/wp-content/uploads/2015/11/1507494017271166356.jpg",
+        "https://i.wpimg.pl/730x0/m.gadzetomania.pl/miecz-14c6db073794e671ea51daed97.jpg",
+
+
+
+    ]
+    return render_template('galeria.html', listaObrazkow=listaObrazkow, title="Galeria")
 
